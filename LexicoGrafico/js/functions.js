6ce0dc9b083138248be7aea1a9fb16e61947a7ce
Rodolfo Padro <rodolfopv94@gmail.com>
2017-02-08 02:24:47 +0000

    Split text

--- LexicoGrafico/js/functions.js
+++ LexicoGrafico/js/functions.js
@@ -1,6 +1,16 @@
-function doFunction(){
-  var lines = $('textarea').val().split('\n," "');
-  for(var i = 0;i < lines.length;i++){
-    console.log(lines[i]);
-  }
+const terminales = ['class', 'program', '{', '}', '(', ')', 'if', 'else', 'while', 'iterate', 'void', 'isRed', 'isBlack', 'isHeart', 'isClubs', 'isDiamond', 'isSpades', 'isNotRed', 'isNotBlack', 'isNotHeart', 'isNotClubs', 'isNotDiamond', 'isNotSpades', 'isEmpty', 'isNotEmpty', '<', '>', '<=', '>=', '==', '!=', 'flip', 'getCard', 'putCard', 'VALUE'];
+
+const text =  () => {
+  //Separa el input en un arreglo en las condiciones que se dan
+  let lines = $('textarea').val().split(/[\s," ",\n]+/);
+  return lines;
+}
+
+console.log(text);
+
+
+function exigir(token){
+  var flag = false;
+  
+  return flag;
 }
